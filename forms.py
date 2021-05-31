e3d78ee36d5065a6c8a6e7c26b6becfcdeaa3c2d
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-05-31 14:27:30 -0500

    created UserEditForm class

--- forms.py
+++ forms.py
@@ -18,6 +18,18 @@ class UserAddForm(FlaskForm):
     image_url = StringField('(Optional) Image URL')
 
 
+class UserEditForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = TextAreaField('(Optional) User Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+
 class LoginForm(FlaskForm):
     """Login form."""
 
